63fd0c7e6466a87249ea747766702f319aad4488
VivekThazhathattil <tvivek@iisc.ac.in>
2022-10-02 14:42:28 +0530

    comment relevant code

--- script.py
+++ script.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import sys
 
+# suppress warnings: mainly deprecated pandas append function warning
 if not sys.warnoptions:
     import warnings
     warnings.simplefilter("ignore")
 
+# define the input and output files here
 DATA_DIR = "data"
 INPUT_FILENAME = "input1.xlsx"
 OUT_FILE_NAME = "output1.xlsx"
@@ -12,14 +14,15 @@ OUT_FILE_NAME = "output1.xlsx"
 # read input excel file
 df = pd.read_excel(DATA_DIR + "/" + INPUT_FILENAME, index_col=None);
 
-# From the header, figure out the distinct test names: concept test #num, 
-# full test #num etc and put it in a list
+# get the headers of the dataframe
 headers = [str(header).strip() for header in df.columns]
 
 tests = [] # list to store test names
 test_details = [] # list to store test specific details
 non_test_details = [] # list to store userid, chapter-tag etc
 
+# collect the test names, various test statistics and other details and 
+#store them in the lists - tests, test_details, non_test_details
 for header in headers:
     if("test" in header.lower()):
         tests.append(header.partition('-')[0].strip().lower())
@@ -27,33 +30,35 @@ for header in headers:
     else:
         non_test_details.append(header.strip().lower())
 
+# filter out duplicate names and sort the list strings lexicographically
 unique_tests = sorted(list(set(tests)))
 unique_test_details = sorted(list(set(test_details)))
 
+
+# function to get the column name with matching substring
 def get_matching_key(keys, match):
     for key in keys:
         if match.lower() in key.lower():
             return key
     return ""
 
+# function to get the column name based on two matching substrings
 def get_matching_key2(keys, match1, match2):
     for key in keys:
         if match1.lower() in key.lower() and match2.lower() in key.lower():
             return key
     return ""
 
+# get the column name for Names of the student column
 names_key =get_matching_key(df.columns, "name")
 
+# get the student names (Potential problem of duplicate names not handled)
 students = [name for name in df[names_key]]
 
+# define result dataframe which will be returned at the end
 result_df = pd.DataFrame()
 
-def append_row(df, row):
-    return pd.concat([
-                df,
-                pd.DataFrame([row], columns=row.index)]
-           ).reset_index(drop=True)
-
+# collect the test stats for each test of each student in separate rows
 for student_no in range(len(students)):
     for test in unique_tests: 
         skip_flag = False
@@ -75,4 +80,5 @@ for student_no in range(len(students)):
         if not skip_flag:
             result_df = result_df.append(row, ignore_index=True)
 
+# output the result in a new xlsx file
 result_df.to_excel(DATA_DIR + "/" + OUT_FILE_NAME, index=False)
